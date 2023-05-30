5298eda91fc4bca043dfa6be7d98d4e270bf3b85
菠菜 <277148066@qq.com>
2023-05-30 18:51:21 +0800

    update todo 1110

---
+++ leetcode.cn/1110 删点成林.js
@@ -0,0 +1,26 @@
+// 1110. 删点成林
+
+// TODO
+
+// 给出二叉树的根节点 root，树上每个节点都有一个不同的值。
+
+// 如果节点值在 to_delete 中出现，我们就把该节点从树上删去，最后得到一个森林（一些不相交的树构成的集合）。
+
+// 返回森林中的每棵树。你可以按任意顺序组织答案。
+
+// 输入：root = [1,2,3,4,5,6,7], to_delete = [3,5]
+// 输出：[[1,2,null,4],[6],[7]]
+
+// 输入：root = [1,2,4,null,3], to_delete = [3]
+// 输出：[[1,2,4]]
+
+// 递归
+const delNodes = (root, to_delete) => {
+  1;
+};
+
+const root = [1, 2, 3, 4, 5, 6, 7];
+
+const { array2TreeNode } = require('../lib/utils');
+
+console.log(delNodes(array2TreeNode(root)));
